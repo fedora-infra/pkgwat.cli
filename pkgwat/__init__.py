230333a974a9210f5e9fe973ee8bc1e41759028a
Ralph Bean <rbean@redhat.com>
2012-06-27 13:08:18 -0400

    Bump.

--- pkgwat/__init__.py
+++ pkgwat/__init__.py
@@ -1,5 +1,5 @@
 __name__ = 'pkgwat'
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 __description__ = "CLI tool for querying the fedora packages webapp"
 __author__ = "Ralph Bean"
 __author_email__ = "rbean@redhat.com"
