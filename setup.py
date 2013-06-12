2e85136c74731783b992d072b994267d1839310f
Ralph Bean <rbean@redhat.com>
2013-06-11 20:16:23 -0400

    "pkgwat history" command.

--- setup.py
+++ setup.py
@@ -41,6 +41,7 @@ subcommands = [
     'dependencies = pkgwat.cli.subcommands:Dependencies',
     'history = pkgwat.cli.subcommands:History',
     'dependencies = pkgwat.cli.subcommands:Dependencies',
+    'history = pkgwat.cli.subcommands:History',
 ]
 
 if sys.version_info[0] == 2:
