14a7d71d43f016e6f14cadb7b8892330b4a7973c
Ralph Bean <rbean@redhat.com>
2013-10-24 11:54:40 -0400

    Remove dupes.

--- setup.py
+++ setup.py
@@ -39,8 +39,6 @@ subcommands = [
     'changelog = pkgwat.cli.subcommands:Changelog',
     'history = pkgwat.cli.subcommands:History',
     'dependencies = pkgwat.cli.subcommands:Dependencies',
-    'history = pkgwat.cli.subcommands:History',
-    'dependencies = pkgwat.cli.subcommands:Dependencies',
 ]
 
 if sys.version_info[0] == 2:
